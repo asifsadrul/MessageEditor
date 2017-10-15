4fa6836fd382882041ca4e42143fc3418f2d0ccc
Asif-Sadrul <sadrul.islam.asif@gmail.com>
2017-10-14 20:17:27 -0700

    Handle bar button

--- MessageEditor/UseCases/MessageEditor/MessageEditorViewController.swift
+++ MessageEditor/UseCases/MessageEditor/MessageEditorViewController.swift
@@ -10,26 +10,27 @@ import UIKit
 
 class MessageEditorViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  @IBOutlet weak var placeholderLbl: UILabel!
+  @IBOutlet weak var messageTextView: UITextView!
+  let wordOfTheDay = "morning"
+  var customBarBTn: UIButton!
+  
+  override func viewDidLoad() {
+      super.viewDidLoad()
+      handleRightBarBtn()
+  }
+  
+  func handleRightBarBtn() {
+    let button = UIButton(frame: CGRect(x: 0, y: 0, width: 24, height: 24))
+    button.setImage(UIImage(named: "settings"), for: UIControlState.normal)
+    button.addTarget(self, action:#selector(goToSettings), for: .touchUpInside)
+    let barButton = UIBarButtonItem(customView: button)
+    self.navigationItem.rightBarButtonItem = barButton
+  }
+  
+  func goToSettings() {
+    self.performSegue(withIdentifier: "settings", sender: self)
+  }
+  
 
 }
